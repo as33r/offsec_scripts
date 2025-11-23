cbed74e9545bc37c83f1a6c15d66439a208868f3
Asmu Deen <47429919+as33r@users.noreply.github.com>
2025-11-23 13:59:51 +0530

    Add S3 XML URL extraction script
    This script fetches and parses URLs from an S3 XML file, checking their status codes and saving them to a text file.

---
+++ s3xmlurl_extract.py
@@ -0,0 +1,24 @@
+"""
+A simple python script to parse all the urls form a S3 XML file.
+"""
+import requests
+import bs4
+import lxml
+import sys
+
+if len(sys.argv) > 1 :
+    s3_url = sys.argv[1]
+else:
+    print("Please give s3 url ")
+    exit(1)
+
+req = requests.get(s3_url)
+xml = bs4.BeautifulSoup(req.text, 'lxml')
+keys = xml.find_all('key')
+keys_data = [key.text for key in keys]
+
+with open('s3_urls.txt', 'a+') as file:
+    for id in keys_data:
+        url = s3_url + id
+        st = requests.head(url).status_code
+        file.write("[" + str(st) + "] " + url + "\n")
